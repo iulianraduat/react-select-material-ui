8fdbcc8dabf9677378cf2571a4a169ecb2638cba
Iulian Radu <iulian.radu@gmx.at>
2018-10-21 20:30:38 +0200

    Adde unit test for SelectLabel

---
+++ src/SelectLabel.test.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { ShallowWrapper, shallow } from "enzyme";
+import InputLabel, { InputLabelProps } from "@material-ui/core/InputLabel/InputLabel";
+import SelectLabel from "./SelectLabel";
+import { colorNoFocus, colorFocus } from "./ColorConstants";
+
+describe("SelectLabel", () => {
+  it("does not render if label is missing", () => {
+    const wrapper: ShallowWrapper = shallow(<SelectLabel />);
+    expect(wrapper.html()).toBeNull();
+  });
+
+  it("uses the label", () => {
+    const label: string = "label";
+    const wrapper: ShallowWrapper = shallow(<SelectLabel label={label} />);
+    expect(
+      wrapper
+        .find(InputLabel)
+        .childAt(0)
+        .text()
+    ).toEqual(label);
+  });
+
+  it("passes id to child", () => {
+    const id: string = "id";
+    const label: string = "label";
+    const wrapper: ShallowWrapper = shallow(<SelectLabel id={id} label={label} />);
+    expect(wrapper.find(InputLabel).prop("htmlFor")).toEqual(id);
+  });
+
+  it("passes shrink to child", () => {
+    const label: string = "label";
+    let shrink: boolean = true;
+    let wrapper: ShallowWrapper = shallow(<SelectLabel label={label} shrink={shrink} />);
+    expect(wrapper.find(InputLabel).prop("shrink")).toEqual(shrink);
+
+    shrink = false;
+    wrapper = shallow(<SelectLabel label={label} shrink={shrink} />);
+    expect(wrapper.find(InputLabel).prop("shrink")).toEqual(shrink);
+  });
+
+  it("uses the correct color for (no) focus", () => {
+    const label: string = "label";
+    let hasInputFocus: boolean = true;
+    let wrapper: ShallowWrapper = shallow(<SelectLabel label={label} hasInputFocus={hasInputFocus} />);
+    expect(wrapper.find(InputLabel).prop("style").color).toEqual(colorFocus);
+
+    hasInputFocus = false;
+    wrapper = shallow(<SelectLabel label={label} hasInputFocus={hasInputFocus} />);
+    expect(wrapper.find(InputLabel).prop("style").color).toEqual(colorNoFocus);
+  });
+
+  it("passes inputLabelProps to child", () => {
+    const label: string = "label";
+    const inputLabelProps: InputLabelProps = { disabled: true, required: true };
+    let wrapper: ShallowWrapper = shallow(<SelectLabel label={label} inputLabelProps={inputLabelProps} />);
+    expect(wrapper.find(InputLabel).props()).toMatchObject(inputLabelProps);
+  });
+});
